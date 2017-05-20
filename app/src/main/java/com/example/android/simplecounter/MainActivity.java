9f58af985ce54c5f7b60dba0f2b3f84bbd16ea1c
masive <masive@abv.bg>
2017-05-20 09:51:49 +0300

    Rename layout

--- app/src/main/java/com/example/android/simplecounter/MainActivity.java
+++ app/src/main/java/com/example/android/simplecounter/MainActivity.java
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void basketballScreen(View view) {
-        setContentView(R.layout.basketball_layout);
+        setContentView(R.layout.basketball);
 
     }
 
